37c27db0b654a3c69a74f2e37a11e0cc830f0a8c
Amaan Qureshi <amaanq12@gmail.com>
2023-06-15 18:00:47 -0400

    chore: update neovim queries

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,42 +1,27 @@
+; References
+
+(identifier) @reference
+
+; Definitions
+
 (function_definition
   (function_output
     [(identifier) @definition.var
-                  (multioutput_variable
-                    (identifier) @definition.var
-                    ("," (identifier) @definition.var))])?
-  function_name: (identifier) @definition.function
+                  (matrix (row
+                    (identifier) @definition.var))])?
+  name: (identifier) @definition.function
   (function_arguments
     (identifier)* @definition.parameter
     ("," (identifier) @definition.parameter)*)?) @scope
 
 (assignment
-  variable: (_) @definition.var)
+  left: (identifier) @definition.var)
 (assignment
-  (multioutput_variable
-    (_) @definition.var))
+  left: (multioutput_variable
+    (identifier) @definition.var))
 
-(unary_operator (identifier) @reference)
-(binary_operator (identifier) @reference)
-(comparison_operator (identifier) @reference)
-(boolean_operator (identifier) @reference)
-(postfix_operator (identifier) @reference)
-(not_operator (identifier) @reference)
-(metaclass_operator (identifier) @reference)
-(handle_operator (identifier) @reference)
-
-(function_call (identifier) @reference)
-(arguments (identifier) @reference)
-
-(struct . [(function_call
-             name: (identifier) @reference)
-           (identifier) @reference])
-
-(range (identifier) @reference)
-(condition (identifier) @reference)
 (iterator . (identifier) @definition.var)
-(iterator (identifier) @reference)
-(parfor_options (identifier) @reference)
 (lambda (arguments (identifier) @definition.parameter))
 (global_operator (identifier) @definition.var)
 (persistent_operator (identifier) @definition.var)
-(catch (captured_exception) @definition.var)
+(catch (identifier) @definition)
--- queries/tags.scm
+++ queries/tags.scm
@@ -1,11 +1,10 @@
  (class_definition
-   class_name: (identifier) @name) @definition.class
+   name: (identifier) @name) @definition.class
 
 (function_definition
-  function_name: (identifier) @name) @definition.function
+  name: (identifier) @name) @definition.function
 
 (function_call
   name: (identifier) @name) @reference.call
 
-(command
-  name: (command_name) @name) @reference.call
+(command (command_name) @name) @reference.call
