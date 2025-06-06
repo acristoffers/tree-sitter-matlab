6dfbc67f5c3f44ec020eda5863850e163b042136
Álan Crístoffer <acristoffers@startmail.com>
2025-06-06 18:20:09 +0200

    abi(14): version 1.0.5

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 1273
 #define LARGE_STATE_COUNT 76
 #define SYMBOL_COUNT 189
@@ -3845,7 +3845,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 15, .external_lex_state = 2},
   [2] = {.lex_state = 14, .external_lex_state = 2},
@@ -47901,7 +47901,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_matlab(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -47930,13 +47929,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_matlab(void) {
       tree_sitter_matlab_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "matlab",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 1,
-      .minor_version = 0,
-      .patch_version = 4,
-    },
   };
   return &language;
 }
