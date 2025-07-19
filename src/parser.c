1e6b761752f3a41ce9551e8b1e425c5ed45257f4
Álan Crístoffer <acristoffers@startmail.com>
2025-07-19 13:23:25 +0200

    abi(14): version 1.0.8

--- src/parser.c
+++ src/parser.c
@@ -51659,13 +51659,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_matlab(void) {
       tree_sitter_matlab_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "matlab",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 1,
-      .minor_version = 0,
-      .patch_version = 6,
-    },
   };
   return &language;
 }
