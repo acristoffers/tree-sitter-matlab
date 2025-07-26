69b3f440689bd37d1ec8bc9b2decb732a37ffd43
Álan Crístoffer <acristoffers@startmail.com>
2025-07-26 19:22:45 +0200

    fix: support the end keyword in ranges
    Closes #45
    
    It broke because it used to be parsed as an identifier and now
    identifiers don't allow keywords anymore, so it had to be explicitly
    added to the grammar.

--- grammar.js
+++ grammar.js
@@ -145,6 +145,7 @@ module.exports = grammar({
           $.string,
           $.field_expression,
           $.unary_operator,
+          alias('end', $.identifier),
         ),
       ),
 
@@ -427,6 +428,7 @@ module.exports = grammar({
         $.string,
         prec.dynamic(-1, $.unary_operator),
         prec.dynamic(1, $.binary_operator),
+        alias('end', $.identifier),
       ),
     range: ($) =>
       prec.right(
@@ -443,14 +445,14 @@ module.exports = grammar({
     continue_statement: (_) => 'continue',
     break_statement: (_) => 'break',
 
-    elseif_clause: ($) =>
+    elseif_clause: ($) => prec.left(
       seq(
         'elseif',
         field('condition', $._expression),
         repeat($._end_of_line),
         optional($.block),
-      ),
-    else_clause: ($) => seq('else', repeat($._end_of_line), optional($.block)),
+      )),
+    else_clause: ($) => prec.left(seq('else', repeat($._end_of_line), optional($.block))),
     if_statement: ($) =>
       seq(
         'if',
@@ -496,16 +498,15 @@ module.exports = grammar({
         'end',
       ),
 
-    case_clause: ($) =>
+    case_clause: ($) => prec.left(
       seq(
         'case',
         // MATLAB says it should be a `switch_expr`, but then accepts any expression
         field('condition', $._expression),
         repeat1($._end_of_line),
         optional($.block),
-      ),
-    otherwise_clause: ($) =>
-      seq('otherwise', repeat($._end_of_line), optional($.block)),
+      )),
+    otherwise_clause: ($) => prec.left(seq('otherwise', repeat($._end_of_line), optional($.block))),
     switch_statement: ($) =>
       seq(
         'switch',
@@ -684,13 +685,13 @@ module.exports = grammar({
         'end',
       ),
 
-    catch_clause: ($) =>
+    catch_clause: ($) => prec.left(
       seq(
         'catch',
         optional(alias($._catch_capture, $.identifier)),
         repeat1($._end_of_line),
         optional($.block),
-      ),
+      )),
     try_statement: ($) =>
       seq(
         'try',
