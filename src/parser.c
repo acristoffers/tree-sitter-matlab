a6394d898df062d429decb15bd794bcfdd859302
Álan Crístoffer <acristoffers@startmail.com>
2025-10-05 18:05:52 +0200

    fix: allow spaces after command name
    Fixes #94

--- src/parser.c
+++ src/parser.c
@@ -73510,7 +73510,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_matlab(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 1,
-      .minor_version = 1,
+      .minor_version = 2,
       .patch_version = 0,
     },
   };
